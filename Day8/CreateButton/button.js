f7671f73f7d7cce1c0b8e549b604d3e3f9a23d35
VivekMalhan666 <amalhan43@gmail.com>
2020-03-14 20:11:36 +0530

    10daysofJavaScript

---
+++ Day8/CreateButton/button.js
@@ -0,0 +1,7 @@
+var button = document.getElementById("btn");
+var count = button.value;
+button.addEventListener("click", function (e) {
+    count++;
+    button.value = count;
+    button.textContent = count.toString();
+}, false);
